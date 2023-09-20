3e7ab18b1414f6ab212e3d1d64c2e93355a1c62b
Thepropollo <111026218+Thepropollo@users.noreply.github.com>
2023-09-19 22:11:02 -0500

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -138,7 +138,8 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-MEDIA_ROOT = 'MEDIA'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/public/'
 
 # Default primary key field type
